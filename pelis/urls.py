46901023d645936deda86996ccc5cb68805b47ea
lmfundora <lmfundora2@gmail.com>
2023-12-02 09:35:17 -0500

    set Labels views

--- pelis/urls.py
+++ pelis/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from pelis.views import Pelis, PeliDetail
+from pelis.views import Pelis, PeliDetail, Label, LabelDetail
 
 urlpatterns = [
     path('', Pelis.as_view()),
-    path('<str:pk>', PeliDetail.as_view())
+    path('<str:pk>', PeliDetail.as_view()),
+    path('/labels/', Label.as_view()),
+    path('/labels/<str:pk>', LabelDetail.as_view()),
 ]
--- pelis/views.py
+++ pelis/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status, generics
-from pelis.models import PelisModel
+from pelis.models import PelisModel, LabelsModel
 from pelis.serializers import PelisSerializer, LabelSerializer
 import math
 
@@ -52,28 +52,89 @@ class PeliDetail(generics.GenericAPIView):
     def get(self, request, pk):
         peli = self.get_peli(pk=pk)
         if peli == None:
-            return Response({"status": "fail", "message": f"Note with Id: {pk} not found"}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"status": "fail", "message": f"Peli with Id: {pk} not found"}, status=status.HTTP_404_NOT_FOUND)
 
         serializer = self.serializer_class(peli)
-        return Response({"status": "success", "data": {"note": serializer.data}})
+        return Response({"status": "success", "data": {"peli": serializer.data}})
 
     def patch(self, request, pk):
-        note = self.get_peli(pk)
-        if note == None:
-            return Response({"status": "fail", "message": f"Note with Id: {pk} not found"}, status=status.HTTP_404_NOT_FOUND)
+        peli = self.get_peli(pk)
+        if peli == None:
+            return Response({"status": "fail", "message": f"Peli with Id: {pk} not found"}, status=status.HTTP_404_NOT_FOUND)
 
         serializer = self.serializer_class(
-            note, data=request.data, partial=True)
+            peli, data=request.data, partial=True)
         if serializer.is_valid():
             # serializer.validated_data['updatedAt'] = datetime.now()
             serializer.save()
-            return Response({"status": "success", "data": {"note": serializer.data}})
+            return Response({"status": "success", "data": {"peli": serializer.data}})
         return Response({"status": "fail", "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
-        note = self.get_peli(pk)
-        if note == None:
-            return Response({"status": "fail", "message": f"Note with Id: {pk} not found"}, status=status.HTTP_404_NOT_FOUND)
+        peli = self.get_peli(pk)
+        if peli == None:
+            return Response({"status": "fail", "message": f"Peli with Id: {pk} not found"}, status=status.HTTP_404_NOT_FOUND)
 
-        note.delete()
+        peli.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class Label(generics.GenericAPIView):
+    serializer_class = LabelSerializer
+    queryset =LabelsModel.objects.all()
+
+
+    def get(self, request):
+        label = LabelsModel.objects.all()
+        serializer = self.serializer_class(label, many=True)
+        return Response({
+            "status": "success",
+            "label": serializer.data
+        })
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": {"label": serializer.data}}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"status": "fail", "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+class LabelDetail(generics.GenericAPIView):
+    queryset = LabelsModel.objects.all()
+    serializer_class = LabelSerializer
+
+    def get_label(self, pk):
+        try:
+            return LabelsModel.objects.get(pk=pk)
+        except:
+            return None
+
+    def get(self, request, pk):
+        label = self.get_label(pk=pk)
+        if label == None:
+            return Response({"status": "fail", "message": f"label with Id: {pk} not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.serializer_class(label)
+        return Response({"status": "success", "data": {"label": serializer.data}})
+
+    def patch(self, request, pk):
+        label = self.get_label(pk)
+        if label == None:
+            return Response({"status": "fail", "message": f"Label with Id: {pk} not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.serializer_class(
+            label, data=request.data, partial=True)
+        if serializer.is_valid():
+            # serializer.validated_data['updatedAt'] = datetime.now()
+            serializer.save()
+            return Response({"status": "success", "data": {"label": serializer.data}})
+        return Response({"status": "fail", "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        label = self.get_label(pk)
+        if label == None:
+            return Response({"status": "fail", "message": f"Label with Id: {pk} not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        label.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
