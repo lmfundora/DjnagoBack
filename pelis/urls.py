1affa8355ae42954a81054e67a0448610e190c5f
lmfundora <lmfundora2@gmail.com>
2023-12-07 12:35:45 -0500

    add pelis urls

--- pelis/urls.py
+++ pelis/urls.py
@@ -4,6 +4,6 @@ from pelis.views import Pelis, PeliDetail, Label, LabelDetail
 urlpatterns = [
     path('', Pelis.as_view()),
     path('<str:pk>', PeliDetail.as_view()),
-    path('/labels/', Label.as_view()),
-    path('/labels/<str:pk>', LabelDetail.as_view()),
+    path('labels/', Label.as_view()),
+    path('labels/<str:pk>', LabelDetail.as_view()),
 ]
